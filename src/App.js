8127b86f5a223d352e4637837d726e451e522b70
Bishal <btthapa82@gmail.com>
2022-12-18 20:44:49 +0545

    Building And Using A custom Context Provider Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,41 +7,47 @@ import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const ctx = useContext(AuthContext);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const storedUserLogInInfo = localStorage.getItem("isLoggedIn");
-    if (storedUserLogInInfo === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const storedUserLogInInfo = localStorage.getItem("isLoggedIn");
+  //   if (storedUserLogInInfo === "1") {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem("isLoggedIn", "1");
+  //   setIsLoggedIn(true);
+  // };
 
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
+  // const logoutHandler = () => {
+  //   localStorage.removeItem("isLoggedIn");
+  //   setIsLoggedIn(false);
+  // };
 
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler
-      }}
-    >
-      <MainHeader  onLogout={logoutHandler} />
+    // <AuthContext.Provider
+    //   value={{
+    //     isLoggedIn: isLoggedIn,
+    //     onLogout: logoutHandler
+    //   }}
+    //>
+    <React.Fragment>
+      <MainHeader />
+      {/* // onLogout={logoutHandler} */}
       {/* isAuthenticated={isLoggedIn} */}
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login />}
+        {/* onLogin={loginHandler} */}
+        {ctx.isLoggedIn && <Home />}
+        {/* onLogout={logoutHandler} */}
       </main>
-    </AuthContext.Provider>
+      {/* // </AuthContext.Provider> */}
+    </React.Fragment>
   );
 }
 
